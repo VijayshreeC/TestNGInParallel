eedafdf631870f169db97e46f2ac8743422f63f4
VijayshreeC <vijayshreechaturvedi@gmail.com>
2020-05-28 10:19:59 +0000

    Update TestUtil.java

--- src/main/java/com/globomantics/test/utilities/TestUtil.java
+++ src/main/java/com/globomantics/test/utilities/TestUtil.java
@@ -17,7 +17,7 @@ public class TestUtil extends TestBase{
 	public  String takeSnapShotAtEnd(String TestCaseName) throws Exception
 	{
 		File scrFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);//cast driver to screenshot mode
-		String currentDir="/var/jenkins_home/workspace/TestProject1" ; //System.getProperty("user.dir");
+		String currentDir="var/jenkins_home/workspace/TestProject1" ; //System.getProperty("user.dir");
 		
 		String destPath= currentDir+"/screenshots/"+System.currentTimeMillis()+TestCaseName+".png";
 		//String a=currentDir+"/screenshots";
