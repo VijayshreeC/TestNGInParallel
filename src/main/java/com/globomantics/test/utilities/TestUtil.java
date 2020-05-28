160e3c4b767542a0b79c409ba18cea6cd91c87bb
VijayshreeC <vijayshree.chaturvedi@citiustech.com>
2020-05-28 18:40:01 +0530

    location update

--- src/main/java/com/globomantics/test/utilities/TestUtil.java
+++ src/main/java/com/globomantics/test/utilities/TestUtil.java
@@ -14,7 +14,7 @@ public class TestUtil extends TestBase{
 	public static long Page_Load_time=160;
 	public static long Implicit_wait_time=100;
 	
-	public  String takeSnapShotAtEnd(String TestCaseName) throws Exception
+	public String takeSnapShotAtEnd(String TestCaseName) throws Exception
 	{
 		File scrFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);//cast driver to screenshot mode
 		//String currentDir="/var/jenkins_home/workspace/TestProject1/reports" ; //System.getProperty("user.dir");
@@ -22,14 +22,11 @@ public class TestUtil extends TestBase{
 		String destPath= "reports/screenshots/"+System.currentTimeMillis()+TestCaseName+".png";
 		//String a=currentDir+"/screenshots";
 		//System.out.println("This screenshot path", a);
+		String imagePath="screenshots/"+System.currentTimeMillis()+TestCaseName+ ".png";
+		
 		File file =new File(destPath);
 		FileUtils.copyFile(scrFile,file);//copies the file from source to the target
-		return destPath;
+		return imagePath;
 	}
 		
 }
-
-/*
-TakeScreenshot ts=(TakeScreenshot)driver;
-File src=ts.getScreenshotAs(OutputType.FILE)
-File*/
