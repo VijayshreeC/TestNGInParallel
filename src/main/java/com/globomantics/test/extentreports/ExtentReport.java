31e33e071b8406ef275fa14fd0e09cc528d25d61
VijayshreeC <vijayshree.chaturvedi@citiustech.com>
2020-05-29 18:12:12 +0530

    Added file patterns

--- src/main/java/com/globomantics/test/extentreports/ExtentReport.java
+++ src/main/java/com/globomantics/test/extentreports/ExtentReport.java
@@ -3,6 +3,9 @@ package com.globomantics.test.extentreports;
 
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
@@ -12,8 +15,9 @@ public class ExtentReport {
 
 	public static ExtentReports ReportGenerator() {
 		extent = new ExtentReports();
-
-		String path = "/var/jenkins_home/workspace/TestProject1/reports/index.html";
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// time stamp
+		String repName = "Test-Report-" + timeStamp + ".html";
+		String path = "/var/jenkins_home/workspace/TestProject/reports/ " + repName;
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
 		reporter.config().setReportName("Globmantic");
 		reporter.config().setDocumentTitle("TestResults");
@@ -22,7 +26,7 @@ public class ExtentReport {
 		//reporter.config().setTheme(Theme.DARK);
 
 		//reporter.loadXMLConfig(new File("/var/jenkins_home/workspace/TestProject1/extent-config.xml"));
-		extent = new ExtentReports();
+		
 
 		extent.attachReporter(reporter);
 		//extent.loadConfig();
