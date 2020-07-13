182788d5c5a5e73b05e64e9d9a2bd5850c91442c
VijayshreeC <50708207+VijayshreeC@users.noreply.github.com>
2020-07-13 14:37:35 +0530

    Update TestBase.java

--- src/main/java/com/globomantics/test/base/TestBase.java
+++ src/main/java/com/globomantics/test/base/TestBase.java
@@ -65,7 +65,7 @@ public class TestBase {
 
 			dc.setPlatform(Platform.LINUX);
 			try {
-				driver1.set(new RemoteWebDriver(new URL("http://35.193.91.97:4444/wd/hub"), dc));// http://34.70.254.170:4444
+				driver1.set(new RemoteWebDriver(new URL("http://35.232.146.10:4444/wd/hub"), dc));// http://34.70.254.170:4444
 				// http://34.77.44.43:4444
 			} catch (MalformedURLException e) {
 
